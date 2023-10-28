4b6c7f0468f0cecf5757466bbc67d68ce5fba315
Heitor Juliani <134893946+niccoti@users.noreply.github.com>
2023-10-28 14:35:18 -0300

    Add files via upload

---
+++ example/assets/js/multilang.js
@@ -0,0 +1,21 @@
+cl()
+function cl() {
+  const script = document.querySelector("script");
+  const langValue = document.querySelector("select").value;
+  const src = script.getAttribute("src");
+  fetch(src)
+    .then(response => response.text())
+    .then((data) => {
+      const obj = JSON.parse(data);
+      const lang = langValue;
+      const data2 = obj[lang];
+
+      for (const [id, text] of Object.entries(data2)) {
+        const element = document.querySelector(`#${id}`);
+        element.textContent = text;
+      }
+    });
+}
+
+
+
